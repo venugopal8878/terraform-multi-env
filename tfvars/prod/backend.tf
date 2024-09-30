d7b645073765620edc3220339740d4e2ca170fef
venugopal <mallavallivenu@gmail.com>
2024-09-30 23:01:07 +0530

    all

---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket ="venus3-prod"  #s3 bucket name
+key ="venu.remotestate-demo" #just specify the key for we user different key for diffrentr repos and we don't use this for another repository
+region ="us-east-1"
+dynamodb_table ="venu-prod"
---
+++ tfvars/prod/prod.tfvars
@@ -0,0 +1,11 @@
+instances = {
+mysql-prod = "t3.small"
+frontend-prod = "t3.micro"
+beckend-prod = "t3.micro"
+}
+
+tags = {
+    Environment = "prod"
+}
+
+environment = "prod"
